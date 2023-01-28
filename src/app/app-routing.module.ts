505eb51c9b0ffa6d8fc1b760e74b67f9e78a0706
rk <102965074+ralphk336@users.noreply.github.com>
2023-01-28 20:14:19 +0300

    added signup form to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AllAlumniComponent } from './all-alumni/all-alumni.component';
 import { AlumniBetweenComponent } from './alumni-between/alumni-between.component';
 import { CustomizeComponentComponent } from './customize-component/customize-component.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 const routes: Routes = [
-  {path: '' , component: AllAlumniComponent},
+  //{path: '' , component: AllAlumniComponent},
+  {path: '' , component: SignupFormComponent},
   {path: 'allAlumni',component: AllAlumniComponent},
   {path: 'alumniBetween',component: AlumniBetweenComponent},
   {path: 'customize',component: CustomizeComponentComponent}
