eaff58086bf2a9137845044ba6846b5930382926
King_Kobia <kobiacaa@gmail.com>
2023-01-30 08:15:00 +0300

    landingPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AccordionModule } from 'primeng/accordion';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+
+//import { Checkboxmodule } from 'primeng/checkbox';
+//import { RadioButtonModule } from 'primeng/radioButton';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    AccordionModule,
+    PanelModule,
+    ButtonModule
+    //CheckboxModule
+    //RadioButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
